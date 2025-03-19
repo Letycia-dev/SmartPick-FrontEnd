c70e0812a79b9de8853f58ae1b099c5e6be07c83
Letycia-dev <167436088+Letycia-dev@users.noreply.github.com>
2025-03-19 20:14:09 -0300

    correção do código Input
    Correção do código para renderização do formulário para cadastro de sorteios

--- smart_pick/src/Pages/Cadastro/Casdastro.jsx
+++ smart_pick/src/Pages/Cadastro/Casdastro.jsx
@@ -1,55 +1,50 @@
-import React from 'react'
-import Style from './Cadastro.module.css'
+import React from 'react';
+import Style from './Cadastro.module.css';
 
 import Input from '../../Form/Input';
 import Button from '../../Form/Button';
 
 function Cadastro() {
 
+
+  
   return (
     <div className={Style.body}>
       <h1>Cadastrar</h1>
 
-        <form>
+      <form>
+        <Input
+          type="text"
+          name="txt_categoria"
+          id="txt_categoria"
+          placeholder="Digite o nome da categoria"
+        />
 
-          
-          <Input
-            type="Text"
-            name="txt_categoria"
-            id="txt_categoria"
-            placeholder="Digite o nome da categoria"
-            />
+        <Input 
+          type="text"
+          name="txt_item_1"
+          id="txt_item_1"
+          placeholder="Digite o nome do item"
+        />
 
-          <Input 
-            type="Text"
-            name="txt_item"
-            id="txt_item"
-            placeholder="Digite o nome do item"
-            />
+        <Input 
+          type="text"
+          name="txt_item_2"
+          id="txt_item_2"
+          placeholder="Digite o nome do item"
+        />
 
-          <Input 
-            type="Text"
-            name="txt_item"
-            id="txt_item"
-            placeholder="Digite o nome do item"
-            />
+        <Input 
+          type="text"
+          name="txt_item_3"
+          id="txt_item_3"
+          placeholder="Digite o nome do item"
+        />
 
-          <Input 
-            type="Text"
-            name="txt_item"
-            id="txt_item"
-            placeholder="Digite o nome do item"
-            />
-
-          <Button
-            label="Cadastrar Sorteio"
-            />
-
-        </form>
-
+        <Button label="Cadastrar Sorteio" />
+      </form>
     </div>
   );
-
-};
+}
 
 export default Cadastro;
